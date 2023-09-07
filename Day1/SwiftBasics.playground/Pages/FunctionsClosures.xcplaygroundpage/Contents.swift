21f78b988b95c5003895582c7a64aea631263f28
Abdulaziz Albahar <abdulaziz@albahar.net>
2023-09-06 20:52:24 -0400

    comments added heavily

--- Day1/SwiftBasics.playground/Pages/FunctionsClosures.xcplaygroundpage/Contents.swift
+++ Day1/SwiftBasics.playground/Pages/FunctionsClosures.xcplaygroundpage/Contents.swift
@@ -1,28 +1,43 @@
-// Creating a function
+
+
+// Creating a function:
 func sayHello(name: String, anotherPerson: String) -> String {
-    return "Hello \(name), and \(anotherPerson)"
+    return "Hello \(name), and \(anotherPerson)" // Same as return"Hello " + name + "and " + anotherPerson.
 }
 
-sayHello(name: "John", anotherPerson: "Mark")
+print(sayHello(name: "John", anotherPerson: "Mark"))
 
 // Functions are types in Swift. The above function's type is (String, String) -> String.
-// Since Functions are types, you can assign them to variables
+// Since Functions are types, you can assign them to variables.
 
-var helloFunc = sayHello
-helloFunc("John", "Mark")
+var helloFunc: (String, String) -> String = sayHello
+print(helloFunc("John", "Mark"))
 
-// explicit and implicit parameter values
+/*
+ We can specify alternative parameter names for when functions are called. This is simply a design choice for more readable and context-driven code.
+ It's a matter of convenience.
+ Let's look at the first String parameter in anotherSayHello(...), 'to' is the parameter name we chose for function calls, 'person' is the name we chose to refer to in the implementation:
+ - To refer to our first String variable from within the function implementation we must use the variable name 'person'.
+ - When making them call to this function elsewhere in your code, that same parameter variable will be displayed as 'to'.
+ See the examples below to understand more.
+ */
 func anotherSayHello(to person: String, and aFriend: String) -> String {
     return "Hello \(person), and \(aFriend)"
 }
 
+print(anotherSayHello(to: "Noah", and: "Max"))
+
 // Since functions are types and can be assigned to variables, you can also pass them as parameters.
-// These becomes "closures".
+// These become "Closures" as they are called. It's a widely used Swift convention. This is important to understand, as a lot of APIs take in "Closures".
 
 func sayHiAndPerformAnOperation(name: String, operation: (Int) -> Int) -> String {
     return "Hello \(name). The result of your operation(5) is \(operation(5))."
 }
 
+
+/*
+ The syntax 'myInt in' is simply the way we instantiate the parameters that we are expecting to receive, which in this case is an integer - look above. 'myInt' seems kind of random, because it is. We can even call this 'myInteger' or 'myInt2'. It doesn't matter.
+ */
 sayHiAndPerformAnOperation(name: "John", operation: { myInt in
     return myInt + 5
 })
@@ -33,7 +48,7 @@ sayHiAndPerformAnOperation(name: "John", operation: { myInt in
     myInt + 5
 })
 
-// Syntactic Sugar Step 2: Remove last parameter name
+// Syntactic Sugar Step 2: Remove last parameter name ('operation')
 
 sayHiAndPerformAnOperation(name: "John") { myInt in
     myInt + 5
@@ -41,7 +56,7 @@ sayHiAndPerformAnOperation(name: "John") { myInt in
 
 // Syntactic Sugar Step 3: Remove parameter name in closure
 
-sayHiAndPerformAnOperation(name: "John") { $0 + 5 }
+sayHiAndPerformAnOperation(name: "John") { $0 + 5 } // Since our function only has one parameter, we can use $0 to refer to it.
 
 
 
--- Day1/SwiftBasics.playground/Pages/Loops.xcplaygroundpage/Contents.swift
+++ Day1/SwiftBasics.playground/Pages/Loops.xcplaygroundpage/Contents.swift
@@ -1,18 +1,20 @@
 
+// LOOPS
+
 let names = ["Anna", "Alex", "Brian", "Jack"]
 
-// for-in loops
+// For-in loops
 for name in names {
     print("Hello, \(name)!")
 }
 
-// for-in with dictionaries -> (key, value)
+// For-in with dictionaries -> (key, value)
 let numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
 for (animalName, legCount) in numberOfLegs {
     print("\(animalName)s have \(legCount) legs")
 }
 
-// for-in with ranges.
+// For-in with ranges.
 for index in 1...5 {
     print("\(index) times 5 is \(index * 5)")
 }
@@ -27,7 +29,10 @@ for index in 1...5 {
 
 let hours = 12
 let hourInterval = 3
-// for-in with stride function.
+
+// For-in with stride function.
 for tickMark in stride(from: 3, through: hours, by: hourInterval) {
-    // Renders the tick mark every 3 hours (3, 6, 9, 12).
+    // Creates a range of numbers from 3 to 12 (hourInterval) with a spacing of 3 (hours) -> [3, 6, 9, 12].
+    print(tickMark)
 }
+
